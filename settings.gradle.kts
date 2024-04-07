ddeaff0d454899f95881494ceb41141e4d34a4e3
Sleepless Dog <di.burmistrov@gmail.com>
2024-04-07 19:19:37 +0400

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PlaylistMaker_ver2"
+include(":app")
+ 
