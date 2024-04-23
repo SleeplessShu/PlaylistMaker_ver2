6d462440fb3d2522a5c728ecf4f031d041236c2c
Sleepless Dog <di.burmistrov@gmail.com>
2024-04-23 18:04:31 +0400

    Добавлено: - Функция сохранения текста в окне поиска при повороте экрана

--- app/src/main/java/com/practicum/playlistmaker_ver2/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_ver2/SearchActivity.kt
@@ -3,12 +3,14 @@ package com.practicum.playlistmaker_ver2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.ImageView
 import com.google.android.material.textfield.TextInputEditText
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchEditText: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -18,8 +20,10 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val searchEditText = findViewById<TextInputEditText>(R.id.search_edit_text)
-
+        searchEditText = findViewById<TextInputEditText>(R.id.search_edit_text)
+        if (savedInstanceState != null) {
+            searchEditText.setText(savedInstanceState.getString("SEARCH_QUERY"))
+        }
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -30,6 +34,11 @@ class SearchActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
             }
         })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SEARCH_QUERY", searchEditText.text.toString())
     }
 
     override fun onPause() {
