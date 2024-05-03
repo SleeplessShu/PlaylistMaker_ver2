f54da7f1edb28781a39293455804f2c93054790b
Sleepless Dog <di.burmistrov@gmail.com>
2024-05-03 22:46:04 +0400

    Исправлено: - цвет статусбара соответствует цвету бэкграунда в каждой активити

--- app/src/main/java/com/practicum/playlistmaker_ver2/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_ver2/SearchActivity.kt
@@ -1,13 +1,17 @@
 package com.practicum.playlistmaker_ver2
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.random.Random
@@ -22,6 +26,15 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
 
         val onPressBackToMain = findViewById<LinearLayout>(R.id.buttonBackToMain)
         onPressBackToMain.setOnClickListener {
--- app/src/main/java/com/practicum/playlistmaker_ver2/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_ver2/SettingsActivity.kt
@@ -2,13 +2,17 @@ package com.practicum.playlistmaker_ver2
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.content.ContextCompat
 
 class SettingsActivity : AppCompatActivity() {
     companion object {
@@ -27,6 +31,15 @@ class SettingsActivity : AppCompatActivity() {
             NIGHT_MODE_KEY,
             AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
         )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
         switcherTheme.isChecked = isNightModeOn
         switcherTheme.setOnCheckedChangeListener { _, isChecked ->
             AppCompatDelegate.setDefaultNightMode(
